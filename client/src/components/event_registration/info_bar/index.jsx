d3c7d1b6216f54c0884e93ee508984e5b0aa5a93
NarayanaRavada <lakshminarayanaravada777@gmail.com>
2022-12-30 11:24:15 +0530

    added features: login modal

--- client/src/components/event_registration/info_bar/index.jsx
+++ client/src/components/event_registration/info_bar/index.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 
 import './info_bar.styles.scss';
 
 const InfoBar = () => {
   return (
-    <div className='d-flex bg-light flex-md-row flex-column rounded border mb-5 px-4'>
-      <div className='d-flex reg-d p-2'>
-        <i class='bi bi-calendar2-x'></i>
-        <h6 className='mx-3 d-flex flex-column'>
+    <div className="d-flex bg-light flex-md-row flex-column rounded border mb-5 px-4">
+      <div className="d-flex reg-d p-2">
+        <i className="bi bi-calendar2-x"></i>
+        <h6 className="mx-3 d-flex flex-column">
           <strong>Registration Deadline</strong> March 12, 2022
         </h6>
       </div>
-      <div className='d-flex align-items-center'>
-        <i class='bi bi-people-fill'></i>
-        <h6 className='mx-3'>Individual Participation</h6>
+      <div className="d-flex align-items-center">
+        <i className="bi bi-people-fill"></i>
+        <h6 className="mx-3">Individual Participation</h6>
       </div>
-      <div className='d-flex align-items-center'>
-        <i class='bi bi-currency-rupee'></i>
-        <h6 className='mx-3'>Free Participation</h6>
+      <div className="d-flex align-items-center">
+        <i className="bi bi-currency-rupee"></i>
+        <h6 className="mx-3">Free Participation</h6>
       </div>
     </div>
   );
--- client/src/components/modal_login/index.jsx
+++ client/src/components/modal_login/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect, useRef, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 
+import { Link, useNavigate } from 'react-router-dom';
 import { useGoogleLogin } from '@react-oauth/google';
 import { useDispatch } from 'react-redux';
 import { signinGoogle } from '../../redux/actions/auth';
@@ -10,41 +10,120 @@ import { signinGoogle } from '../../redux/actions/auth';
 import './modal_login.styles.scss';
 
 const LoginModal = ({ show, handleClose }) => {
-  const handleSignUp = event => {
+  const [data, setData] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const didMount = useRef(false);
+  const showErrors = useRef(false);
+  const modalFirstFocus = useRef(null);
+
+  console.log(data);
+
+  useEffect(() => {
+    isValid();
+  }, [data]);
+
+  useEffect(() => {
+    modalFirstFocus.current?.focus();
+  }, []);
+
+  const setField = (field, value) => {
+    setData({
+      ...data,
+      [field]: value,
+    });
+  };
+
+  const isValid = (field, value) => {
+    const { college, contact } = data;
+    const newErrors = {};
+
+    //contact number erros
+    const contactre = /[0-9]{10}/;
+    if (!contactre.test(contact)) {
+      newErrors.contact = 'Enter a valid mobile number';
+    } else if (contact.length !== 10) {
+      newErrors.contact = 'Mobile number must contain 10 digits';
+    }
+    // college name erros
+    if (!college || college === '') {
+      newErrors.college = 'college name cannot be empty';
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length !== 0;
+  };
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleGoogleLoginSuccess = (tokenResponse) => {
+    const accessToken = tokenResponse.access_token;
+    dispatch(signinGoogle(accessToken, navigate));
+  };
+  const login = useGoogleLogin({ onSuccess: handleGoogleLoginSuccess });
+
+  const handleGoogleSignUp = (event) => {
     event.preventDefault();
+    if (Object.keys(errors).length !== 0) return;
   };
   return (
     <>
       <Modal
         show={show}
         onHide={handleClose}
-        backdrop='static'
+        backdrop="static"
         keyboard={false}
       >
-        <Modal.Header closeButton className='text-white gradient-bg'>
+        <Modal.Header closeButton className="text-white gradient-bg">
           <Modal.Title>E-Summit 2023</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className='d-flex flex-column align-items-center'>
-            <Form onSubmit={handleSignUp}>
-              <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <div className="d-flex flex-column align-items-center">
+            <Form onSubmit={handleGoogleSignUp}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>College/Institute Name</Form.Label>
-                <Form.Control type='email' placeholder='Enter email' />
+                <Form.Control
+                  placeholder="Enter college name"
+                  ref={modalFirstFocus}
+                  isValid={data.college}
+                  isInvalid={
+                    errors.college &&
+                    data.college === '' &&
+                    data.college != null
+                  }
+                  onChange={(event) =>
+                    setField('college', event.target.value.trim())
+                  }
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.college}
+                </Form.Control.Feedback>
               </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicEmail'>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Contact Number</Form.Label>
-                <Form.Control type='tel' placeholder='Enter email' />
+                <Form.Control
+                  type="tel"
+                  isValid={data.contact}
+                  isInvalid={errors.contact && data.contact}
+                  placeholder="Enter contact number"
+                  onChange={(event) =>
+                    setField('contact', event.target.value.trim())
+                  }
+                />
+                <Form.Control.Feedback type="invalid" className="animatedEntry">
+                  {errors.contact}
+                </Form.Control.Feedback>
               </Form.Group>
-
-              <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-                <Form.Check type='checkbox' label='Check me out' />
-              </Form.Group>
-              <button type='submit' class='login-with-google-btn mb-2'>
+              <button type="submit" className="login-with-google-btn mb-2">
                 Sign up with Google
               </button>
+              <p className="tnc">
+                * By Signing up, you agree our <Link>Terms & Conditions</Link>
+              </p>
             </Form>
             or
-            <button type='button' class='login-with-google-btn mt-2'>
+            <button type="button" className="login-with-google-btn my-2">
               Sign in with Google
             </button>
           </div>
